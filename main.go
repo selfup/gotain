e02f9c76c880fb154935f8b13b99122e155d93c5
Regis <boudinot.regis@yahoo.com>
2019-05-09 19:00:13 -0500

    we have liftoff

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		fmt.Println("Nope..")
+	}
+}
+
+func run() {
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	check(cmd.Run())
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
