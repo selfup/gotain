203715b3deca4854d2b7c157fb9046dbfb8c4ffc
selfup <boudinot.regis@yahoo.com>
2019-05-13 09:28:08 -0500

    child exec

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -17,7 +18,7 @@ func main() {
 }
 
 func run() {
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
@@ -30,6 +31,28 @@ func run() {
 	}
 
 	check(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v \n", os.Args[2:])
+
+	cg()
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	check(syscall.Sethostname([]byte("container")))
+	check(syscall.Chroot("/home/liz/ubuntufs"))
+	check(os.Chdir("/"))
+	check(syscall.Mount("proc", "proc", "proc", 0, ""))
+	check(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
+
+	check(cmd.Run())
+
+	check(syscall.Unmount("proc", 0))
+	check(syscall.Unmount("thing", 0))
 }
 
 func check(err error) {
